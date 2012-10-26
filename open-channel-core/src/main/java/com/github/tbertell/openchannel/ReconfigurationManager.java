950e4d8625bbd1dd7702b094d023480fb612e62d
Tom Bertell <tom.bertell@gmail.com>
2012-10-26 15:01:08 +0300

    no comments

--- open-channel-core/src/main/java/com/github/tbertell/openchannel/ReconfigurationManager.java
+++ open-channel-core/src/main/java/com/github/tbertell/openchannel/ReconfigurationManager.java
@@ -6,29 +6,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.github.tbertell.openchannel.channel.model.ChannelVariabilityModel;
 import com.github.tbertell.openchannel.reconfiguration.ReconfigurationHandler;
-import com.github.tbertell.openchannel.reconfiguration.TimerLogChannelReconfigurationHandler;
 
 public class ReconfigurationManager {
 
 	@Autowired
 	private ChannelManager channelManager;
-	
+
 	private static String HANDLER_PACKAGE = "com.github.tbertell.openchannel.reconfiguration";
 
 	public void reconfigure(String channelId, Map<String, String> params) {
-		ChannelVariabilityModel model = channelManager.getChannel(channelId);
+		ChannelVariabilityModel model = null;
+		try {
+			model = channelManager.getChannel(channelId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			// do nothing
+		}
+		if (model != null) {
+			ReconfigurationHandler handler = findHandler(channelId);
 
-		ReconfigurationHandler handler = findHandler(channelId);
-
-		if (handler.isReconfigurationNeeded(params)) {
-			ChannelVariabilityModel newModel = handler.reconfigure(params, model);
-			channelManager.updateChannel(channelId, newModel);
+			if (handler != null && handler.isReconfigurationNeeded(params)) {
+				ChannelVariabilityModel newModel = handler.reconfigure(params, model);
+				channelManager.updateChannel(channelId, newModel);
+			}
 		}
 	}
-	
+
 	private ReconfigurationHandler findHandler(String channelId) {
 		try {
-			Class<ReconfigurationHandler> clazz = (Class<ReconfigurationHandler>) Class.forName(HANDLER_PACKAGE +"." +channelId +"ReconfigurationHandler");
+			Class<ReconfigurationHandler> clazz = (Class<ReconfigurationHandler>) Class.forName(HANDLER_PACKAGE + "."
+					+ channelId + "ReconfigurationHandler");
 			ReconfigurationHandler handler = clazz.newInstance();
 			return handler;
 		} catch (Exception e) {
